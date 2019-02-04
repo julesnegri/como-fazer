072696013007ebe096f74b43af88708a0a488688
unknown <jsoaresandrie@gmail.com>
2019-02-04 06:04:47 -0200

    initial version

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const categorias = require('./routes/categorias')
+const publicacoes = require('./routes/publicacoes')
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded())
+
+const port = process.env.PORT || 3000
+
+app.get('/', async(request, response)  => {
+    //const content = await axios.get('https://como-fazer-jules.firebaseio.com/teste.json')
+    //console.log(content.data)
+    response.render('index')
+})
+
+app.use('/categorias', categorias)
+app.use('/publicacoes', publicacoes)
+
+app.listen(port, (err) => {
+    if(err){
+        console.log('error')
+    }else{
+        console.log('Como-Fazer Server is running on port:', port)
+    }
+})
